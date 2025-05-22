34b9125a222a6bfd6a5371f6ce902b6b826769b2
khawaja naqeeb <shajiekns@yahoo.com>
2025-05-23 01:59:38 +0500

    class iteration program created

---
+++ custom-class-iterable.py
@@ -0,0 +1,18 @@
+class CountDown:
+    def __init__(self,start):
+        self.current =start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < 0:
+            raise StopIteration
+        value=self.current
+        self.current -=1
+        return value
+    
+# using the Countdown  
+for num in CountDown(10):
+    print(num)
+
